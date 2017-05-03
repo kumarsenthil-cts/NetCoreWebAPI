10fc023924b69bb3d04e7755400b9dc6df45d470
kumarsenthil-cts <kumar.senthil@cognizant.com>
2017-05-03 15:21:22 +0530

    :quit

--- src/NetCoreWebAPI/Program.cs
+++ src/NetCoreWebAPI/Program.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration.CommandLine;
+using Microsoft.Extensions.Configuration;
 
 namespace NetCoreWebAPI
 {
@@ -12,7 +14,12 @@ namespace NetCoreWebAPI
     {
         public static void Main(string[] args)
         {
+            var configurations = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
+
             var host = new WebHostBuilder()
+                .UseConfiguration(configurations)
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
